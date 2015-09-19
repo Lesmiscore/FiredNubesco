64023d694ecbf5473200e0f7686272c5f1fd1609
nao20010128nao <nao20010128@gmail.com>
2015-09-19 22:35:06 +0900

    製作開始

---
+++ src/nao20010128nao/MazeCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace nao20010128nao;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+
+use pocketmine\Player;
+
+use pocketmine\utils\TextFormat;
+
+class MazeCommand extends Command implements PluginIdentifiableCommand
+{
+	public function __construct(xSudo $plugin, $name, $description)
+	{
+		$this->plugin = $plugin;
+		parent::__construct($name, $description);
+	}
+	
+	private function checkPermission(CommandSender $sender){
+		if(!($sender->isOp() or $sender->hasPermission("fn.maze"))){
+			$sender->sendMessage(TextFormat::RED . "You don't have permission to use this command.");
+			return false;
+		}
+		return true;
+	}
+	
+	public function execute(CommandSender $sender, $label, array $args)
+	{
+		if(!isset($args[0]) or !isset($args[1]))
+		{
+			if(!$this->checkPermission($sender)) return true;
+			$sender->sendMessage(TextFormat::GREEN . "[FiredNubesco] Usage: /maze <id>[:<damage>] [<wall(inside|outside|none)> [<under(true|false)> [<top(true|false)>]]]");
+			return false;
+		}
+	
+	}
+
+	public function getPlugin(){
+		return $this->plugin;
+	}
+}
